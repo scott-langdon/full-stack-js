f27199e6435198db1eff2dfc35c6a6e088e856bc
Scott Langdon <scott.langdon@tylt.com>
2018-02-02 15:26:16 -0800

    dice roll with constructor and instance

---
+++ object-oriented-programming/constructor-functions-and-prototypes/dice-roll-constructor-and-instance/dice.js
@@ -0,0 +1,10 @@
+function Dice(sides){
+  this.sides = sides; 
+  this.roll = function(){
+    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+    return randomNumber
+  }
+}
+
+var dice = new Dice(6); 
+var dice10 = new Dice(10); 
