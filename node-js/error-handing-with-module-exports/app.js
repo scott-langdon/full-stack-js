b9b419de70f8da90f0a552ea6ea42a08b14a3efa
Scott Langdon <scott.langdon@tylt.com>
2018-02-09 12:52:26 -0800

    module.exports

---
+++ node-js/error-handing-with-module-exports/app.js
@@ -0,0 +1,8 @@
+// Problem: We need a simple way to look at a user's badge count and JavaScript points
+// Solution: Use Node.js to connect to Treehouse's API to get profile information to print out
+
+const profile = require('./profile.js'); 
+
+
+const users = process.argv.slice(2);
+users.forEach(profile.get);
