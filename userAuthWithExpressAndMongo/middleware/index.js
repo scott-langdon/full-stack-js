6849d5897ca9e1c8354fa0be6d01bed74f829eb5
Scott Langdon <scott.langdon@tylt.com>
2018-03-12 22:27:56 -0700

    user auth

---
+++ userAuthWithExpressAndMongo/middleware/index.js
@@ -0,0 +1,20 @@
+function loggedOut(req, res, next) {
+  if (req.session && req.session.userId) {
+    return res.redirect('/profile');
+  }
+  return next();
+}
+
+function requiresLogin(req, res, next) {
+  if (req.session && req.session.userId) {
+    return res.redirect('/login');
+  } else {
+	var err = new Error('You must be logged in to view this page.');
+    err.status = 401;
+    return next(err);
+  }
+  return next();
+}
+
+module.exports.loggedOut = loggedOut; 
+module.exports.requiresLogin = requiresLogin; 
