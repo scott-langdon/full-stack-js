daa6848db79e8fba759968418206d10a2421671c
Scott Langdon <scott.langdon@tylt.com>
2018-03-15 14:27:45 -0700

    completed voting

---
+++ REST-API-with-express/qa-rest-api/mongoose_sandbox.js
@@ -0,0 +1,54 @@
+'use strict'; 
+
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/sandbox');
+
+var db = mongoose.connection;
+
+db.on('error', function(err){
+	console.error("connection error", err);
+});
+
+db.once("open", function(){
+	console.log('db connection successful');
+	// All database communication goes here 
+
+	var Schema = mongoose.Schema; 
+	var AnimalSchema = new Schema({
+		type:  { type: String, default: "goldfish"}, 
+		size:  { type: String, default: "small"},
+		color: { type: String, default: "golden"},
+		mass:  { type: Number, default: 0.007},
+		name:  { type: String  default: "Angela"}
+	});
+
+	var Animal = mongoose.model("Animal", AnimalSchema);
+
+	var elephant = new Animal({
+		type: "elephant",
+		size: "big", 
+		color: "gray",
+		mass: 6000,
+		name: "Lawrence"
+	});
+
+	var animal = new Animal({});
+
+	Animal.remove({}, function(err){
+		elephant.save(function(err){
+			if(err) console.error("Save failed.", err);
+			animal.save(function(err){
+				if(err) console.error("Save Failed.", err);
+				db.close(function(){
+					console.log('db connection closed');
+				});
+			});
+		}); 
+	});
+
+
+}); 
+
+
+
