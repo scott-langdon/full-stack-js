909fc28b48a24d73901005138bbb31ed661a988c
Scott Langdon <scott.langdon@tylt.com>
2018-03-18 11:15:12 -0700

    complete

--- REST-API-with-express/qa-rest-api/app.js
+++ REST-API-with-express/qa-rest-api/app.js
@@ -9,8 +9,21 @@ var jsonParser = require("body-parser").json;
 var logger = require("morgan");
 
 app.use(logger("dev"));
-
 app.use(jsonParser());
+
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/qa');
+
+var db = mongoose.connection;
+
+db.on('error', function(err){
+	console.error("connection error", err);
+});
+
+db.once("open", function(){
+	console.log('db connection successful');
+}); 
 
 app.use("/questions", routes);
 
